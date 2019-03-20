c7e415e4c765826b3629824f2ea449beb07f2e9b
Iggy <igoririanto@rocketmail.com>
2019-03-20 08:02:33 -0500

    add fake json users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,11 @@
 import {Router} from 'express';
 
+// for now, fake json users would suffice
+import users from '../models/users.json';
 const router = Router();
 
 router.get('/', (req, res) => {
+  console.log(users);
   res.send(req.path);
 });
 
